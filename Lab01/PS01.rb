8578ca9c4d5382b03964937f1ea71d4721a8a11f
Mikaila Johnson <MikailaSJohnson@gmail.com>
2011-05-02 20:47:38 -0400

    Complete Successfully Problem Set 1

--- Lab01/PS01.rb
+++ Lab01/PS01.rb
@@ -4,3 +4,9 @@
 # 1. Asks the user to enter his/her first name.
 # 2. Asks the user to enter his/her last name.
 # 3. Prints out the user's first and last name in a greeting.
+
+puts "What is your first name?"
+first_name = gets.chomp
+puts "What is your last name?"
+last_name = gets.chomp
+puts "Hello" + " " + first_name + " " + last_name
